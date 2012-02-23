bde2e6c7ccfeb6ff38e8ebc4d8973b04d349e477
Janne Grunau <j@jannau.net>
2012-02-23 17:27:13 +0100

    LGE star tablet: add mods for drivers/misc/mpu3050/mpu-i2c.h

--- drivers/misc/mpu3050/mpu-i2c.h
+++ drivers/misc/mpu3050/mpu-i2c.h
@@ -33,26 +33,26 @@
 #include <linux/i2c.h>
 
 int sensor_i2c_write(struct i2c_adapter *i2c_adap,
-		     unsigned char address,
-		     unsigned int len, unsigned char const *data);
+		unsigned char address,
+		unsigned int len, unsigned char const *data);
 
 int sensor_i2c_write_register(struct i2c_adapter *i2c_adap,
-			      unsigned char address,
-			      unsigned char reg, unsigned char value);
+			unsigned char address,
+			unsigned char reg, unsigned char value);
 
 int sensor_i2c_read(struct i2c_adapter *i2c_adap,
-		    unsigned char address,
-		    unsigned char reg,
-		    unsigned int len, unsigned char *data);
+		unsigned char address,
+		unsigned char reg,
+		unsigned int len, unsigned char *data);
 
 int mpu_memory_read(struct i2c_adapter *i2c_adap,
-		    unsigned char mpu_addr,
-		    unsigned short mem_addr,
-		    unsigned int len, unsigned char *data);
+		unsigned char mpu_addr,
+		unsigned short mem_addr,
+		unsigned int len, unsigned char *data);
 
 int mpu_memory_write(struct i2c_adapter *i2c_adap,
-		     unsigned char mpu_addr,
-		     unsigned short mem_addr,
-		     unsigned int len, unsigned char const *data);
+		unsigned char mpu_addr,
+		unsigned short mem_addr,
+		unsigned int len, unsigned char const *data);
 
 #endif	/* __MPU_I2C_H__ */
