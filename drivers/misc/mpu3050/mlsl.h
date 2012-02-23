1a6bd64098cdd705d3265cbd6066fb7f61ea1727
Janne Grunau <j@jannau.net>
2012-02-23 17:27:12 +0100

    LGE star tablet: add mods for drivers/misc/mpu3050/mlsl.h

--- drivers/misc/mpu3050/mlsl.h
+++ drivers/misc/mpu3050/mlsl.h
@@ -31,13 +31,11 @@ extern "C" {
 /* - Defines. - */
 /* ------------ */
 
-/*
- * NOTE : to properly support Yamaha compass reads,
- * the max transfer size should be at least 9 B.
- * Length in bytes, typically a power of 2 >= 2
- */
+#if 0
 #define SERIAL_MAX_TRANSFER_SIZE 128
-
+#else
+#define SERIAL_MAX_TRANSFER_SIZE 32
+#endif
 /* ---------------------- */
 /* - Types definitions. - */
 /* ---------------------- */
@@ -52,9 +50,9 @@ extern "C" {
 	tMLError MLSLSerialClose(void *sl_handle);
 
 	tMLError MLSLSerialWriteSingle(void *sl_handle,
-				       unsigned char slaveAddr,
-				       unsigned char registerAddr,
-				       unsigned char data);
+				unsigned char slaveAddr,
+				unsigned char registerAddr,
+				unsigned char data);
 
 	tMLError MLSLSerialRead(void *sl_handle,
 				unsigned char slaveAddr,
@@ -63,31 +61,31 @@ extern "C" {
 				unsigned char *data);
 
 	tMLError MLSLSerialWrite(void *sl_handle,
-				 unsigned char slaveAddr,
-				 unsigned short length,
-				 unsigned char const *data);
+				unsigned char slaveAddr,
+				unsigned short length,
+				unsigned char const *data);
 
 	tMLError MLSLSerialReadMem(void *sl_handle,
-				   unsigned char slaveAddr,
-				   unsigned short memAddr,
-				   unsigned short length,
-				   unsigned char *data);
+				unsigned char slaveAddr,
+				unsigned short memAddr,
+				unsigned short length,
+				unsigned char *data);
 
 	tMLError MLSLSerialWriteMem(void *sl_handle,
-				    unsigned char slaveAddr,
-				    unsigned short memAddr,
-				    unsigned short length,
-				    unsigned char const *data);
+				unsigned char slaveAddr,
+				unsigned short memAddr,
+				unsigned short length,
+				unsigned char const *data);
 
 	tMLError MLSLSerialReadFifo(void *sl_handle,
-				    unsigned char slaveAddr,
-				    unsigned short length,
-				    unsigned char *data);
+				unsigned char slaveAddr,
+				unsigned short length,
+				unsigned char *data);
 
 	tMLError MLSLSerialWriteFifo(void *sl_handle,
-				     unsigned char slaveAddr,
-				     unsigned short length,
-				     unsigned char const *data);
+				unsigned char slaveAddr,
+				unsigned short length,
+				unsigned char const *data);
 
 	tMLError MLSLWriteCal(unsigned char *cal, unsigned int len);
 	tMLError MLSLReadCal(unsigned char *cal, unsigned int len);
@@ -100,4 +98,4 @@ extern "C" {
 /**
  * @}
  */
-#endif				/* MLSL_H */
+#endif /* MLSL_H */
