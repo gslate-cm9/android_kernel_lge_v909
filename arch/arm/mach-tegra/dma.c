dd1639ab6667c87e87fcd4a9eb670b5de82b6c4f
Janne Grunau <j@jannau.net>
2012-03-17 20:37:29 +0100

    tegra/dma: print more APB DMA registers for debugging

--- arch/arm/mach-tegra/dma.c
+++ arch/arm/mach-tegra/dma.c
@@ -1143,6 +1143,11 @@ subsys_initcall(tegra_dma_syscore_init);
 #include <linux/debugfs.h>
 #include <linux/seq_file.h>
 
+#define APB_DMA_STAT     0x04
+#define APB_DMA_TX_REQ   0x08
+#define APB_DMA_RX_REQ   0x0c
+#define APB_DMA_IRQ_STAT 0x14
+#define APB_DMA_TRIG_REG 0x28
 static int dbg_dma_show(struct seq_file *s, void *unused)
 {
 	int i;
@@ -1152,6 +1157,10 @@ static int dbg_dma_show(struct seq_file *s, void *unused)
 			__raw_readl(general_dma_addr + APB_DMA_GEN));
 	seq_printf(s, "DMA_CNTRL: 0x%08x\n",
 			__raw_readl(general_dma_addr + APB_DMA_CNTRL));
+	seq_printf(s, "IRQ_STAT:  0x%08x\n",
+	                __raw_readl(general_dma_addr + APB_DMA_IRQ_STAT));
+	seq_printf(s, "TRIG_REG:  0x%08x\n",
+	                __raw_readl(general_dma_addr + APB_DMA_TRIG_REG));
 	seq_printf(s, "IRQ_MASK:  0x%08x\n",
 			__raw_readl(general_dma_addr + APB_DMA_IRQ_MASK));
 
